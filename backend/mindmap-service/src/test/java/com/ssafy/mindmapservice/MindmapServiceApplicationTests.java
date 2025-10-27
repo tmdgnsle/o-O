c80932e885dfde3128f3c998e30503d5da486c6e
한동근 <ehdrms001030@naver.com>
2025-10-27 16:04:54 +0900

    mindmap project init

---
+++ backend/mindmap-service/src/test/java/com/ssafy/mindmapservice/MindmapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.mindmapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
