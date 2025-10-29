6b183936873650f6468272ddbd09d77781f20c62
박소영 <cindy0585@naver.com>
2025-10-29 11:06:54 +0900

    feat: tailwind, 폰트 css 기본 설정

--- front/o-o/src/App.tsx
+++ front/o-o/src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
-import { Button } from "./shared/ui/button";
+import "./index.css";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="bg-dotted bg-size-dotted flex min-h-svh flex-col items-center justify-center">
+      <p>test</p>
+      <div className="font-paperlogy">
+        <p> 안녕하세요 </p>
+      </div>
     </div>
   );
 }
--- front/o-o/tailwind.config.js
+++ front/o-o/tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // colors
+      colors: {
+        primary: "#263A6B",
+        background: "#FAFAFA",
+        "semi-white": "#F8F9FA",
+        "semi-black": "#333333",
+        "black-gray": "#383737",
+        "deep-gray": "#828181",
+        gray: "#D5D5D5",
+        danger: "#E33B2E"
+      },
+
+      // background
+      backgroundImage: {
+        dotted: 'radial-gradient(#CFCFCF 1px, transparent 0.8px)',
+      },
+      backgroundSize: {
+        dotted: '30px 30px',
+      },
+
+      // font
+      fontFamily: {
+        paperlogy: ["Paperlogy", "system-ui" ,"sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
