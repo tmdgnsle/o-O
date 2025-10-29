8c88419616bbddc3451a6821a8fd29a0afb42795
홍시은 <tjwns53@naver.com>
2025-10-29 09:49:21 +0900

    feat: 폴더 구조 Feature-based 구조로 수정

--- front/o-o/src/App.tsx
+++ front/o-o/src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./shared/ui/button";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
