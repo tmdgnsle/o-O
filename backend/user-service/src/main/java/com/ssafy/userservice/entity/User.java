29a4decdfd089eabd4ec987f5d624084c46e7ccb
한동근 <ehdrms001030@naver.com>
2025-11-03 16:49:52 +0900

    fix: User Table 생성 DDL 변경 - "user"가 PostgreSQL의 예약어로 되어 있는 문제 해결

--- backend/user-service/src/main/java/com/ssafy/userservice/entity/User.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/entity/User.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
