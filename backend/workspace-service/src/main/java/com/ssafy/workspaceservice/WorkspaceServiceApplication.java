3718582e7ac55d6e16fa50c6cf7de01ca0b3f939
김진현 <ajtwlssoqkd@naver.com>
2025-11-03 15:46:37 +0900

    기본 설정

---
+++ backend/workspace-service/src/main/java/com/ssafy/workspaceservice/WorkspaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.workspaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceServiceApplication.class, args);
+    }
+
+}
