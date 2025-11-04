9ace526cd695ba3b70744ff358e407904ab8dd60
홍시은 <tjwns53@naver.com>
2025-11-04 17:41:33 +0900

    feat: 달력 추가
    S13P31D202-95

---
+++ front/o-o/src/features/mypage/components/CalendarDetail.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from "@/components/ui/calendar";
+
+export function CalendarDetail() {
+  return (
+    <div>
+      <Calendar></Calendar>
+      <p>캘린더입니다.</p>
+    </div>
+  );
+}
--- front/o-o/src/features/mypage/components/CalendarView.tsx
+++ front/o-o/src/features/mypage/components/CalendarView.tsx
@@ -1,10 +1,13 @@
+import { CalendarDetail } from "./CalendarDetail";
 import { DashboardTabNav } from "./DashboardTabNav";
 
 export function CalendarView() {
   return (
-    <div className="mx-12 p-7 mt-5 bg-white/60 rounded-3xl">
-      <DashboardTabNav />
-      <p>캘린더뷰</p>
+    <div className="mx-12 px-7 mt-5 bg-white/60 rounded-3xl relative">
+      <div className="absolute z-10 right-5 top-5">
+        <DashboardTabNav />
+      </div>
+      <CalendarDetail />
     </div>
   );
 }
