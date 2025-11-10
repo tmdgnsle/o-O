69c0d818223fa8c08b27af9f150f379a5c61e7b2
홍시은 <tjwns53@naver.com>
2025-11-10 11:14:02 +0900

    refactor: 헤더 수정

--- front/o-o/src/shared/ui/Header.tsx
+++ front/o-o/src/shared/ui/Header.tsx
@@ -41,7 +41,7 @@ export function Header() {
 
   return (
     <>
-      <header className="flex items-center justify-between font-paperlogy px-4 py-3 sm:px-8 sm:py-4 lg:px-16 lg:py-6">
+      <header className="flex items-center justify-between font-paperlogy px-4 py-3 sm:px-8 sm:py-6 lg:px-16 lg:py-12">
         <div className="flex items-center gap-4 sm:gap-8">
           <Link to="/">
             <img
