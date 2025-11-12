fa32a8059760c1d325a79d8d2cce1f1e042ab472
홍시은 <tjwns53@naver.com>
2025-11-12 15:35:16 +0900

    fix: 헤더에 닉네임 길 경우, 말줄임표로 변경

--- front/o-o/src/shared/components/Header/UserProfile.tsx
+++ front/o-o/src/shared/components/Header/UserProfile.tsx
@@ -20,8 +20,8 @@ export function UserProfile({
             className="w-9 h-9 lg:w-11 lg:h-11 object-cover rounded-full"
           />
         </div>
-        <span className="text-sm lg:text-base font-medium text-primary">
-          {userName}
+        <span className="text-sm lg:text-base font-medium text-primary truncate">
+          {userName.length > 10 ? `${userName.slice(0, 10)}…` : userName}
         </span>
       </div>
     </button>
