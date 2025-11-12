355e54505eda66f9df432cbf645c7e10b0d70089
홍시은 <tjwns53@naver.com>
2025-11-12 22:30:41 +0900

    !hotfix: reissue 테스트

--- front/o-o/src/features/auth/pages/CallbackPage.tsx
+++ front/o-o/src/features/auth/pages/CallbackPage.tsx
@@ -23,7 +23,7 @@ export function CallbackPage() {
         // 1. URL에서 token과 userId 추출
         // const token = searchParams.get("token");
         const token =
-          "eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJJZCI6NCwicm9sZSI6IlVTRVIiLCJwbGF0Zm9ybSI6IndlYiIsImlhdCI6MTc2Mjk1MjQzNSwiZXhwIjoxNzYyOTUyNjE1fQ.w89uGzGTgTy1WnUsav4rzo6cYrdwho5sTC9AwxI0Uso";
+          "eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJJZCI6NCwicm9sZSI6IlVTRVIiLCJwbGF0Zm9ybSI6IndlYiIsImlhdCI6MTc2Mjk1NDIxOSwiZXhwIjoxNzYyOTU0Mzk5fQ.5ADCUrUjBvS12AfKY-lXYyPyf0ODxr9MHTVzMwCVG9M";
         const userId = searchParams.get("userId");
 
         console.log("📥 Callback 받음:", { token, userId });
