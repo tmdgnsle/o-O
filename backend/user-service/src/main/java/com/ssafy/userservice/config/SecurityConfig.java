30253215596df3736c3bf28f59fcb5f008669ee1
한동근 <ehdrms001030@naver.com>
2025-11-12 17:50:20 +0900

    feat: user-service에서 쿠키 받아오도록 cors 설정

--- backend/user-service/src/main/java/com/ssafy/userservice/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +19,7 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -54,8 +56,8 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
-        // CORS 설정
-        http.cors(AbstractHttpConfigurer::disable);  // Gateway에서 처리
+        // CORS 설정 (쿠키 전송을 위해 활성화)
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource));
 
         return http.build();
     }
