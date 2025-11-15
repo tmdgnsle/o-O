305c468f19cf53cac7e047a6b2dc9c246857fa3e
송진우 <rkddkwl059@naver.com>
2025-11-16 03:04:28 +0900

    feat : trend search mindmap 부르지 않고 elastic search로 대체

---
+++ backend/trend-service/src/main/java/com/ssafy/trendservice/config/ElasticsearchConfig.java
@@ -0,0 +1,31 @@
+// trend-service: src/main/java/com/ssafy/trendservice/config/ElasticsearchConfig.java
+package com.ssafy.trendservice.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(HttpHost.create(esHost)).build();
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(RestClient restClient) {
+        ElasticsearchTransport transport =
+                new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
+}
